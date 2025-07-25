7d165f0f7a58ec1fc347401c9e876ce97e959969
Sally M <smei201900@gmail.com>
2025-07-24 21:06:38 -0400

    started editing the JS file

---
+++ apps.js
@@ -0,0 +1,15 @@
+document.getElementById("bookForm").addEventListener("submit", function(event)); 
+event.preventDefault() 
+//this should prevent the default form submission activity 
+
+const bookTitle = document.getElementById("Book_Title").value; 
+const bookAuthor = document.getElementById("Book_Author").value;
+const bookPublisher = document.getElementById("Book_Publisher").value;
+
+
+
+
+
+
+
+
