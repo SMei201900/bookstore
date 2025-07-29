943e00c4debb3721804b41144415b2e1776efc25
Sally M <smei201900@gmail.com>
2025-07-29 19:26:07 -0400

    added api

--- apps.js
+++ apps.js
@@ -1,33 +1,50 @@
-//https://bookstore-api-six.vercel.app/ 
+document.addEventListener("DOMContentLoaded", () {
+    fetch('https://bookstore-api-six.vercel.app/api/books').then(response => {
+        if (!response.ok) {
+            throw new Error("Failed to fetch book data");
+        } return response.json(); 
+    }).then(data => {
+        const outputArea = document.getElementById("output_Area");
+        //
+        data.forEach(book => {
+            const newRow = document.createElement("tr");
+        
+            newRow.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.publisher || "NA"}</td>
+            `; 
+
+            //this allows us to add the new row (aka "tr" and the tds) into the existing table at the end 
+            outputArea.appendChild(newRow); 
+        }); 
+    }).catch(error => {
+        console.error("Error loading books:", error)
+    });
+});
+
 
 document.getElementById("bookForm").addEventListener("submit", function(event) {
     event.preventDefault();
 
-/*we look for our form via the id: bookForm 
-    We add an eventListener, type: submit
-    we create a function called "event"
-    .preventDefault will prevent the default submission activity
-*/
+    const bookTitle = document.getElementById("Book_Title").value; 
+    const bookAuthor = document.getElementById("Book_Author").value;
+    const bookPublisher = document.getElementById("Book_Publisher").value;
 
-const bookTitle = document.getElementById("Book_Title").value; 
-const bookAuthor = document.getElementById("Book_Author").value;
-const bookPublisher = document.getElementById("Book_Publisher").value;
-const outputArea = document.getElementById("output_Area"); 
+    const newRow = document.createElement("tr");
 
-/*we want our information to become a new row in the table*/
-const newRow = document.createElement("tr") 
+    newRow.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.publisher || "NA"}</td>
+        `; 
 
-/*since we created a new_row just above, we just add the tds now*/
-newRow.innerHTML = `
-    <td>${bookTitle}</td>
-    <td>${bookAuthor}</td>
-    <td>${bookPublisher}</td>
-    `
+    outputArea.appendChild(newRow); 
 
-//this allows us to add the new row (aka "tr" and the tds) into the existing table at the end 
-outputArea.appendChild(newRow)
+    document.getElementById("bookForm").reset() //clear the form after submission 
 
-});
+})
+
 /*everything had to be INSIDE the eventListener 
     b/c without it, we are appending empty strings
     AKA we were submitting the form when there was nothing
